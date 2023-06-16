6e514ecec158da3c16666412e520e324759fee96
장현종 <43039077+JUNdie26@users.noreply.github.com>
2023-06-16 13:40:12 +0900

    Update BoxJump.cs

--- BoxJump.cs
+++ BoxJump.cs
@@ -6,21 +6,13 @@ using UnityEngine;
 public class BoxJump : MonoBehaviour
 {
     protected float Animation;
-
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        /*
-        Animation += Time.deltaTime;
-        Animation - Animation % 5;
-        transform.position - MathParabola.Parabola(Vector3.zero, Vector3.forward * 10f, 5f, Animation / 5f);
-        */
         if (Input.GetMouseButtonDown(0))
             GetComponent<ParabolaController>().FollowParabola();
     }
